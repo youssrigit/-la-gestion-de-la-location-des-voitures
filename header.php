855b6e39f625a74b9e57e2b93dcf18c401bf5348
boussaidimohamed <mohamed.boussaidi.1997@gmail.com>
2018-05-04 10:34:11 +0200

    modifier les  fichiers header et index

--- header.php
+++ header.php
@@ -1,18 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Catatan Si Boy a Bootstarp  Website Template | Home :: w3layouts</title>
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,800,700' rel='stylesheet' type='text/css'>
-<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-<script src="js/jquery.min.js"></script>                
-</head>
-<body>
+
 <!-- header -->
     <div class="banner">
         <div class="container">
