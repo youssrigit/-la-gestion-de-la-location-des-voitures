6af1f26af1a75b4ff3be4d9b33bdfbc5e7a59068
boussaidimohamed <mohamed.boussaidi.1997@gmail.com>
2018-05-04 10:23:18 +0200

    ajouter le fichier footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- footer -->
+	<div class="footer">
+		<div class="container">
+			<p>Template by <a href="http://w3layouts.com" target="_blank"> w3layouts</a></p>
+			<div class="social">
+				<ul>
+					<li><a href="#"><i class="fb"> </i></li></a>
+					<li><a href="#"><i class="twt"> </i></li></a>
+						<div class="clearfix"></div>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!-- footer -->
