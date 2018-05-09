cac6666740e2c264d4191785343716ace1a8f096
boussaidimohamed <mohamed.boussaidi.1997@gmail.com>
2018-05-10 00:38:59 +0200

    crees les page content-single et singel

--- index.php
+++ index.php
@@ -21,20 +21,16 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <script src="js/jquery.min.js"></script>                
 </head>
 <body>
-	<?php get_header(); ?>
-    <div class="row">
-    <div class="col-sm-8 blog-main">
-        <?php if ( have_posts() ) : while ( have_posts() ) :
-            the_post(); get_template_part( 'content', get_post_format() );
-        endwhile;
-    ?>
-        <nav>
-            <ul class="pager">
-                <li><?php next_posts_link( 'Précédent' ); ?></li>
-                <li><?php previous_posts_link( 'Suivant' ); ?></li>
-            </ul>
-        <?php   endif; ?> </div> <!-- /.blog-main -->
+	
+
+<?php get_header(); ?>
+<div class="row">
+ <div class="col-sm-8 blog-main">
+ <?php if ( have_posts() ) : while ( have_posts() ) :
+the_post(); get_template_part( 'content', get_post_format() );
+endwhile; endif; ?> </div> <!-- /.blog-main -->
 
 <?php get_footer(); ?>
+
 </body>
 </html>
