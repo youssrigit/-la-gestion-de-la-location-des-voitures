eeec12efc3b39d053f1ebdf9b1f69f72f0fbeaba
boussaidimohamed <mohamed.boussaidi.1997@gmail.com>
2018-05-10 00:36:10 +0200

    ajouter les codes php pour afficher les posts a la page content

--- content.php
+++ content.php
@@ -9,12 +9,29 @@
 				<h5>by <a href="#">Samidi ,</a> 27 July 2013 ,<a href="#">Traveling</a></h5>
 				<p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. </p>
 			</div>
+
+<div class="blog-post">
+ <h2 class="blog-post-itle"><?php the_title(); ?></h2>
+  <p class="blog-post-meta"><?php the_date(); ?> par <a
+href="#"><?php the_author(); ?></a></p>
+ <?php the_content(); ?>
+</div><!-- /.blog-post -->
+
+
 			<div class="col-md-1 travelling2">
 				<h3>2</h3>
 				<a href="#">comments</a>
 			</div>
 				<div class="clearfix"> </div>
 	</div>
+
+
+<br>
+<h4>Archives</h4> <ol class="list-unstyled"> <?php
+wp_get_archives( 'type=monthly' ); ?> </ol> 
+<h4>About</h4> <p><?php the_author_meta( 'description' ); ?>
+</p> 
+
 	<div class="lequotes">
 		
 			<div class="col-md-12">
